fc2770b28c87e4785bd663cda5f0450841e3409b
Lucsposito <83612022+Lucsposito@users.noreply.github.com>
2021-12-02 13:30:46 -0300

    Add files via upload

---
+++ DesafioCatapulta.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace dotnetcore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pedras = 4, bateria, minutos, pedrasLascadas;
+
+        Console.WriteLine("Digite a duração da bateria");
+        minutos = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite a quantidade de  bateria");
+        bateria = int.Parse(Console.ReadLine());
+
+        pedrasLascadas = minutos * pedras * bateria;
+        Console.WriteLine($"A quantidade de pedras Lançadas será de: {pedrasLascadas}.");
+        }
+    }
+}
